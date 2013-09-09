d10e0ad42c4e4ffcae06dcf5076efcad45e2b5e4
MerelyAPseudonym <MerelyAPseudonym@gmail.com>
2013-09-09 14:31:12 -0400

    plt-racket 5.3.6

--- Library/Formula/plt-racket.rb
+++ Library/Formula/plt-racket.rb
@@ -2,21 +2,22 @@ require 'formula'
 
 class PltRacket < Formula
   homepage 'http://racket-lang.org/'
-  # Use GitHub tarball as the release tarball doesn't have
-  # everything needed for building on OS X
-  url 'https://github.com/plt/racket/archive/v5.2.tar.gz'
-  sha1 '313425909ff8d956c3e99d0a2b5b3cb12d0f98ad'
+  url 'https://github.com/plt/racket/archive/v5.3.6.tar.gz'
+  sha256 'aa33ff303a8b7f7fed28a1d78e0a38760c1fe33faf7a1d365ee1ba7d3da1ec11'
+  
+  depends_on 'glib'
+  depends_on 'cairo'
+  depends_on 'pango'
+  depends_on 'jpeg'
 
   def install
     cd 'src' do
-      args = ["--disable-debug", "--disable-dependency-tracking",
-              "--enable-xonx",
-              "--enable-shared",
-              "--prefix=#{prefix}" ]
-
-      if MacOS.prefer_64_bit?
-        args += ["--enable-mac64", "--enable-sgc", "--disable-gracket"]
-      end
+      args = %W[ --disable-debug --disable-dependency-tracking
+                 --enable-xonx
+                 --prefix=#{prefix}
+                 --disable-gracket ]
+                 
+      args += '--disable-mac64' unless MacOS.prefer_64_bit?
 
       system "./configure", *args
       system "make"
