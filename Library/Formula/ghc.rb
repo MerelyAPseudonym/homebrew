a981401d1f7c11310087f63d8bd018c031425fb2
Josh Tilles <MerelyAPseudonym@gmail.com>
2014-05-06 15:03:05 -0400

    Prefer &&/||/! over and/or/not.
    Q.v. https://github.com/Homebrew/homebrew/pull/28689#discussion_r11978058

--- Library/Formula/ghc.rb
+++ Library/Formula/ghc.rb
@@ -15,8 +15,8 @@ class Ghc < Formula
   depends_on :macos => :snow_leopard
 
   # GHC 7.6 —which we might use for bootstrapping— still needs GCC
-  depends_on "gcc" => :build if (build.build_32_bit? or
-                                 not MacOS.prefer_64_bit? or
+  depends_on "gcc" => :build if (build.build_32_bit? ||
+                                 !MacOS.prefer_64_bit? ||
                                  MacOS::Xcode.version < "5.1")
 
   resource "7.8.2-x86_64-binary" do
@@ -43,7 +43,7 @@ class Ghc < Formula
     # Move the main tarball contents into a subdirectory
     (buildpath+"Ghcsource").install Dir["*"]
 
-    if (build.build_32_bit? or not MacOS.prefer_64_bit?)
+    if (build.build_32_bit? || !MacOS.prefer_64_bit?)
       binary_resource = "7.6.3-i386-binary"
       arch = "i386"
     elsif MacOS::Xcode.version < "5.1"
